99a800def3c430e017c2244077555cefdc3f342a
lostheaven142 <lost_heaven142@mail.ru>
2015-01-21 10:19:18 +0000

    инициализация

---
+++ source/assets/javascripts/x.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $('#more_for_post1').click(function(){
+        $('#post1').animate({height:'210px'},500);
+    });
+    $('#more_for_post2').click(function(){
+        $('#post2').animate({height:'210px'},500);
+    });
+    $('#more_for_post3').click(function(){
+        $('#post3').animate({height:'210px'},500);
+    });
+    $('#more_for_post4').click(function(){
+        $('#post4').animate({height:'210px'},500);
+    });
+});
